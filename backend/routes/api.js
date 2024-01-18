487b8a7158cf151ae81ff9f7e2e356cbb673f8ae
braym3 <60962768+mbray3@users.noreply.github.com>
2024-01-18 12:21:42 +0000

    Converted endpoints to async

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -17,42 +17,60 @@ let polls = [
 let votes = []; // saving votes to a list - no persistance
 
 // Get a poll by its ID
-router.get('/polls/:pollId', (req, res) => {
+router.get('/polls/:pollId', async (req, res, next) => {
 	const pollId = parseInt(req.params.pollId);
-    const poll = polls.find(poll => poll.pollId === pollId);
-	// if the poll doesn't exist - return an error message
-    if (!poll) {
-		return res.status(404).json({ error: 'Poll not found' });
-    }
-    res.json(poll);
+
+	try{
+		const poll = polls.find(poll => poll.pollId === pollId);
+		// if the poll doesn't exist - return an error message
+		if (!poll) {
+			return res.status(404).json({ error: 'Poll not found' });
+		}
+		res.json(poll);
+	} catch (err) {
+		next({ status: 500, msg: 'Oops an error has occured!'});
+	} 
 });
 
 // Post a vote
-router.post('/votes', (req, res) => {
+router.post('/votes', async (req, res, next) => {
     const { pollId, optionId } = req.body; // destructure out of the body
-    const poll = polls.find(poll => poll.pollId === pollId);
-	// if the poll doesn't exist - return an error message
-    if (!poll) {
-        return res.status(404).json({ error: 'Poll not found' });
-    }
-    const option = poll.options.find(opt => opt.optionId === optionId);
-	// if the chosen option doesn't exist - return an error message
-    if (!option) {
-		return res.status(400).json({ error: 'Invalid option' });
-    }
-	// increment the vote count
-    option.votes++;
-    votes.push({ pollId, optionId });
-	// return a successul response
-    res.json({ success: true });
+
+	try{
+		const poll = polls.find(poll => poll.pollId === pollId);
+		// if the poll doesn't exist - return an error message
+		if (!poll) {
+			return res.status(404).json({ error: 'Poll not found' });
+		}
+		const option = poll.options.find(opt => opt.optionId === optionId);
+		// if the chosen option doesn't exist - return an error message
+		if (!option) {
+			return res.status(400).json({ error: 'Invalid option' });
+		}
+
+		// increment the vote count
+		option.votes++;
+		votes.push({ pollId, optionId });
+		// return a successul response
+		res.status(201).json({ success: true });
+	} catch (err) {
+		next({ status: 500, msg: 'Oops an error has occured!'});
+	}
 });
 
+
+
 // Get all votes for a specific poll by ID
-router.get('/votes/:pollId', (req, res) => {
+router.get('/votes/:pollId', async (req, res, next) => {
 	const pollId = parseInt(req.params.pollId);
-	// filter the votes for the specified poll ID
-	const pollVotes = votes.filter(vote => vote.pollId === pollId);
-	res.json(pollVotes);
+
+	try{
+		// filter the votes for the specified poll ID
+		const pollVotes = votes.filter(vote => vote.pollId === pollId);
+		res.json(pollVotes); 
+	} catch (err) {
+		next({ status: 500, msg: 'Oops an error has occured!'});
+	}
 });
 
 module.exports = router; // export router from the file
