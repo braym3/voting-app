53a504df6dee9d22d13547c18a756745c45bf761
braym3 <60962768+mbray3@users.noreply.github.com>
2024-01-19 12:56:16 +0000

    removed console log from word processor

--- frontend/src/utils/wordProcessor.js
+++ frontend/src/utils/wordProcessor.js
@@ -3,8 +3,5 @@ export const isImportantWord = (word, index, words) => {
     const isUpperCase = /^[A-Z]/.test(word);
     const previousWord = index > 0 ? words[index - 1] : '';
     const isAfterPunctuation = /[.?!]/.test(previousWord);
-
-    console.log(`Word: ${word}, Index: ${index !== undefined ? index : 'undefined'}, isFirstWord: ${isFirstWord}, isUpperCase: ${isUpperCase}, previousWord: ${previousWord}, isAfterPunctuation: ${isAfterPunctuation}`);
-
     return !isFirstWord && isUpperCase && !isAfterPunctuation;
 };
